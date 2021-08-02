d7c5450b8ebf65bb213fe66f56a4b18337b8d38d
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2021-08-03 02:03:01 +0300

    test(http): test base url interceptor

--- src/app/http-interceptors/base-url.interceptor.spec.ts
+++ src/app/http-interceptors/base-url.interceptor.spec.ts
@@ -1,16 +1,25 @@
+import { HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { BaseUrlInterceptor } from './base-url.interceptor';
 
 describe('BaseUrlInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      BaseUrlInterceptor
-      ]
+    providers: [BaseUrlInterceptor],
   }));
 
   it('should be created', () => {
-    const interceptor: BaseUrlInterceptor = TestBed.inject(BaseUrlInterceptor);
+    const interceptor = TestBed.inject(BaseUrlInterceptor);
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should append the url and api key to requests', async () => {
+    const interceptor = TestBed.inject(BaseUrlInterceptor);
+    const nextHandler = jasmine.createSpyObj<HttpHandler>('HttpHandler', ['handle']);
+    nextHandler.handle.and.returnValue(of(new HttpResponse()));
+
+    await expectAsync(interceptor.intercept(new HttpRequest<unknown>('GET', '/search.php'), nextHandler).toPromise());
+    expect(nextHandler.handle).toHaveBeenCalledOnceWith(jasmine.objectContaining({ url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php' }));
   });
 });

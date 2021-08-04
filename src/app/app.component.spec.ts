86291fcb4fb432fafb77e3409231c89947db49e2
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2021-08-04 11:41:22 +0300

    test(app): test theme toggle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,6 +7,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -19,16 +21,36 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render the main element', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('main')).toBeTruthy();
+  });
+
+  it('should toggle between modes', () => {
+    const localStorageSetSpy = spyOn(localStorage, 'setItem');
+
+    const element: HTMLElement = fixture.nativeElement;
+    const toggle = element.querySelector<HTMLInputElement>(
+      'input[type="checkbox"]'
+    )!;
+    toggle.click();
+    fixture.detectChanges();
+
+    expect(localStorageSetSpy).toHaveBeenCalledOnceWith('theme', 'true');
+
+    toggle.click();
+    fixture.detectChanges();
+
+    expect(localStorageSetSpy).toHaveBeenCalledWith('theme', 'false');
   });
 });
